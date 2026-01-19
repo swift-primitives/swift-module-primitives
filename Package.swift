b1393dafbfaeb7e0b7430bbabb57014abed54699
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-19 22:24:31 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-module-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26)
+    ],
+    products: [
+        .library(
+            name: "Module Primitives",
+            targets: ["Module Primitives"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../swift-source-primitives")
+    ],
+    targets: [
+        .target(
+            name: "Module Primitives",
+            dependencies: [
+                .product(name: "Source Primitives", package: "swift-source-primitives")
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety()
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
