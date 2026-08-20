dde665fbca1749fc1a2320879c6d4f2daca01794
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:50 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-module-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,7 +22,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-source-primitives.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-source-primitives.git",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
@@ -35,7 +38,10 @@ let package = Package(
             name: "Module Primitives Test Support",
             dependencies: [
                 "Module Primitives",
-                .product(name: "Source Primitives Test Support", package: "swift-source-primitives"),
+                .product(
+                    name: "Source Primitives Test Support",
+                    package: "swift-source-primitives"
+                ),
             ],
             path: "Tests/Support"
         ),
